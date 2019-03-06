65620dad14754e8ee5a368bd58ce9d8f5addb5ba
ZHENYU HU <feiyeyuji@gamil.com>
2019-03-06 11:19:53 +1100

    stream Proj Online

---
+++ src/actions/index.js
@@ -0,0 +1,58 @@
+import {
+  SIGN_IN,
+  SIGN_OUT,
+  CREATE_STREAM,
+  FETCH_STREAMS,
+  FETCH_STREAM,
+  DELETE_STREAM,
+  EDIT_STREAM
+} from "./types";
+import streamsAPI from "../apis/streamsAPI";
+import history from "../history";
+
+export const signIn = userId => {
+  return {
+    type: SIGN_IN,
+    payload: userId
+  };
+};
+
+export const signOut = () => {
+  return {
+    type: SIGN_OUT
+  };
+};
+
+export const createStream = formValues => async (dispatch, getState) => {
+  const { userId } = getState().auth;
+  const res = await streamsAPI.post("/streams", { ...formValues, userId });
+
+  dispatch({ type: CREATE_STREAM, payload: res.data });
+  history.push("/");
+};
+
+export const fetchStreams = () => async dispatch => {
+  const res = await streamsAPI.get("/streams");
+
+  dispatch({ type: FETCH_STREAMS, payload: res.data });
+};
+
+export const fetchStream = id => async dispatch => {
+  const res = await streamsAPI.get(`/streams/${id}`);
+
+  dispatch({ type: FETCH_STREAM, payload: res.data });
+};
+
+export const editStream = (id, formValues) => async dispatch => {
+  const res = await streamsAPI.patch(`/streams/${id}`, formValues);
+
+  dispatch({ type: EDIT_STREAM, payload: res.data });
+  history.push("/");
+};
+
+export const deleteStream = id => async dispatch => {
+  await streamsAPI.delete(`/streams/${id}`);
+
+  dispatch({ type: DELETE_STREAM, payload: id });
+  history.push("/");
+};
---
+++ src/id.js
@@ -0,0 +1,2 @@
+var clientId =
+  "456939671978-4a3p9hg3oa9shcpq9g04p193jc1asphc.apps.googleusercontent.com";
